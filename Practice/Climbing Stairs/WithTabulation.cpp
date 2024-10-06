130e6feb77a48d77b3122ff7be4adc44968c4183
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-10-06 15:48:03 +0300

    Create WithTabulation.cpp

---
+++ Practice/Climbing Stairs/WithTabulation.cpp
@@ -0,0 +1,17 @@
+//използва вектор (таблица), в която се изчисляват броят стъпки до n във for цикъла, n-тия елемент е резултата
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
