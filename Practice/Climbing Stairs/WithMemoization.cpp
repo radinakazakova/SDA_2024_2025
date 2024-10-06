1a997506ec17d1e5abf145c609b66ada8e318d82
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-10-06 15:44:27 +0300

    Create WithMemoization.cpp

---
+++ Practice/Climbing Stairs/WithMemoization.cpp
@@ -0,0 +1,18 @@
+// използва unordered map, за да запази предишни резултати
+class Solution {
+public:
+    int climbStairs(int n, unordered_map<int, int>& memo) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (memo.find(n) == memo.end()) {
+            memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo); //ако не намери, смята
+        }
+        return memo[n];
+    }
+
+    int climbStairs(int n) {
+        unordered_map<int, int> memo;
+        return climbStairs(n, memo);
+    }
+};
