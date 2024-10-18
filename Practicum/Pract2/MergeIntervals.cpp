9faaf9be6a004e7c300e997159a8f87f8a7c0272
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-10-18 11:20:37 +0300

    Update MergeIntervals.cpp

--- Practicum/Pract2/MergeIntervals.cpp
+++ Practicum/Pract2/MergeIntervals.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         std::sort(intervals.begin(), intervals.end(), [](const vector<int>& f, const vector<int>& s){
-            return f[0] < s[0]; //sortira intervalite
+            return f[0] < s[0]; //sortira intervalite po purva komponenta - po-malkoto chislo 
         });
 
-        vector<vector<int>> merged; //vuv vectora premahvaneto e bavno zatova nov masiv
+        vector<vector<int>> merged; //vuv vectora imlementiranoto premahvane e bavno, zatova nov masiv
         vector<int> curr = intervals[0];
         for(int i = 0; i < intervals.size(); i++)
         {
-            if(curr[1] >= intervals[i][0])
+            if(curr[1] >= intervals[i][0]) //sravnqvame kraq na curr s nachaloto na i-tiq. ako sa ravni syshto suvpadat
             {
                 curr[1] = max(curr[1], intervals[i][1]);
             }
