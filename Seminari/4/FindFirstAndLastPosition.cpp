4ef2075718616610923e7c12e417ba95ac6e7c58
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-10-24 14:42:47 +0300

    Create FindFirstAndLastPosition.cpp

---
+++ Seminari/4/FindFirstAndLastPosition.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int lowerBound(vector<int>& nums, int target)
+    {
+        int res = -1;
+        int left = 0; 
+        int right = nums.size() - 1;
+
+        while(left <= right)
+        {
+            int mid = left + (right - left)/2;
+
+            if(nums[mid] == target)
+            {
+                res = mid;
+                right = mid - 1;
+            }
+            else if(nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+                left = mid + 1;
+        }
+        return res;
+    }
+
+    int upperBound(vector<int>& nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+        int res = -1;
+
+        while(left <= right)
+        {
+            int mid = left +(right - left)/2;
+            if(nums[mid] == target)
+            {
+                res = mid;
+                left = mid + 1;
+            }
+            else if(nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid -1;
+        }
+        return res;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> result(2, -1);
+        result[0] = lowerBound(nums, target);
+        result[1] = upperBound(nums, target);
+        return result;
+    }
+};
