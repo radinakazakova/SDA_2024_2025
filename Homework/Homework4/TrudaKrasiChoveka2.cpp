1a0e9c813e3b71663df5160d4a4d964fc6d8bc67
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-11-10 01:37:55 +0200

    Create TrudaKrasiChoveka2.cpp

---
+++ Homework/Homework4/TrudaKrasiChoveka2.cpp
@@ -0,0 +1,51 @@
+//решението, което първо се опитах да измисля
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
+
+const size_t MAX= 1e6+2;
+size_t N, K;
+
+size_t input[MAX];
+
+int main() {
+    cin>>N>>K;
+    
+    for(size_t i=0; i<N; i++)
+    {
+        cin>>input[i];
+    }
+    
+    queue<size_t> currentNums; // група с размер К
+    size_t currentMin = -1;  
+    for(size_t j = 0; j<K; j++)
+    {
+        currentNums.push(input[j]);
+        currentMin = min(input[j], currentMin);
+    }
+
+    size_t sum = currentMin;
+
+    for(size_t i = K; i<N; i++)
+    {
+        if(currentMin==currentNums.front()) //ако ще махаме сегашния минимум
+        {
+            currentMin = *min_element(input+i-K+1, input+i+1); //преизчисляваме минимума за следващия интервал, +1 защото се мърдаме с 1 от предходния интервал който е (i-k,i)
+        }  
+      
+        currentNums.push(input[i]);
+        currentMin = min(currentNums.back(), currentMin);
+
+        sum+=currentMin;
+        currentNums.pop();  
+     }
+
+    cout<<sum;
+    return 0;
+}
