031048f786873582f0a336c8e9439ecec29bbe4a
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-11-10 15:56:03 +0200

    Create RemovingStarsfromString.cpp

---
+++ Practicum/Pract5/RemovingStarsfromString.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string res;
+
+        for(auto ch: s)
+        {
+            if(ch != '*')
+            {
+                res.push_back(ch);
+            }
+            else
+            {
+                res.pop_back();
+            }
+        }
+        return res;
+    }
+};
