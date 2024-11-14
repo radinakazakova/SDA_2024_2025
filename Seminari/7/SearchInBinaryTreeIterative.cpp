c81c63e70e264dcdc01aa9fb91d0a8ef4abc9b9c
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-11-14 13:36:46 +0200

    Create SearchInBinaryTreeIterative.cpp

---
+++ Seminari/7/SearchInBinaryTreeIterative.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* curr = root;
+
+        while(curr)
+        {
+            if(curr->val == val)
+            {
+                return curr;
+            }
+            if(curr->val > val)
+            {
+                curr = curr->left;
+            }
+            else 
+            {
+                curr = curr->right;
+            }
+        }
+
+        return nullptr;
+    }
+};
