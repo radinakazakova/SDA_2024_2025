a7ac7b0940534140f0f057be4a15cc69d4d93f44
Radina Kazakova <147272490+radinakazakova@users.noreply.github.com>
2024-12-15 15:17:59 +0200

    add another dfs solution from seminar

--- Seminari/11/CloneGraph/dfs.cpp
+++ Seminari/11/CloneGraph/dfs.cpp
@@ -44,3 +44,29 @@ public:
         return dfs(node, clones);
     }
 };
+
+//подобно решение от семинар
+class Solution {
+    // pointer value is a number => can be hashed
+    unordered_map<Node*, Node*> indexToNode;
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node) {
+            return node;
+        }
+        
+        auto iter = indexToNode.find(node);
+        if(iter != indexToNode.end()) {
+            return iter->second;
+        }
+        
+        Node* newNode = new Node(node->val);
+        indexToNode[node] = newNode;
+        
+        for(auto neighbour: node->neighbors) {
+            newNode->neighbors.push_back(cloneGraph(neighbour));
+        }
+        
+        return newNode;
+    }
+};
